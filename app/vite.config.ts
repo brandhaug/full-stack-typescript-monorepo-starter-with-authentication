927dc22a2746e67f133b6da1b8ff37fae44b90f9
Martin Brandhaug <martin@strise.ai>
2022-11-05 20:03:13 +0100

    conf: remove vite base

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import graphql from '@rollup/plugin-graphql'
 
 export default defineConfig({
-  base: '/',
   root: 'src',
   build: {
     outDir: '../dist',
