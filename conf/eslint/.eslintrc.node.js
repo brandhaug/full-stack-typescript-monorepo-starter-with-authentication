c5d59a0d8ef407d64a8578aaf4afa0a211991c77
Martin Brandhaug <martin@brandhaug.net>
2022-12-21 21:42:51 +0100

    conf: node eslint plugin

--- conf/eslint/.eslintrc.node.js
+++ conf/eslint/.eslintrc.node.js
@@ -1,3 +1,14 @@
 module.exports = {
-  extends: ['.eslintrc.base']
+  extends: ['.eslintrc.base', 'plugin:node/recommended'],
+  env: {
+    node: true
+  },
+  rules: {
+    'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }]
+  },
+  settings: {
+    node: {
+      tryExtensions: ['.js', '.ts', '.d.ts']
+    }
+  }
 }
